73d88055b7b9c4020c876c6ec3d47523ae989acb
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2022-12-20 00:05:58 +0900

    add anntaion

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -4,6 +4,12 @@ import { GqlExecutionContext } from '@nestjs/graphql';
 import { User } from 'src/users/entities/user.entity';
 import { AllowedRoles } from './role.decorator';
 
+//인증 절차(authguard)
+// CanActivate true와 false만 return하면서 request를 진행할 지 결정함
+// 1. (!roles) : metadata가 있는 지 확인(권한이 무엇인지 확인)
+// 2. (!user) : 로그인되었는 지 확인
+// 위 둘을 가지고 true or false return하여 request 진행할지 결정
+
 @Injectable()
 export class AuthGuard implements CanActivate {
   // CanActivate return only true or false
@@ -16,15 +22,18 @@ export class AuthGuard implements CanActivate {
   // https://docs.nestjs.com/guards#putting-it-all-together
   canActivate(context: ExecutionContext) {
     const roles = this.reflector.get<AllowedRoles>(
-      // AllowedRoles : frome role.decorator
+      // AllowedRoles : from role.decorator
       // reflector : check metadata
       'roles',
-      // import from resolver decorater @Role(['Delivery])
+      // 'role' is role.decorator Role's metadata key
+      // for importing from resolver decorater @Role(['Delivery])
       context.getHandler(),
     );
     // console.log(roles);
     if (!roles) {
       return true;
+      // roles가 없다면 metadata가 없다는 것이니까 public(권한없어도 됨)을 의미함
+
       // role is undefined, canAcitve return true
       // no metadata return true
       // if no decorator in resolver method, return true
@@ -33,12 +42,16 @@ export class AuthGuard implements CanActivate {
     // graphQL의 ExcutionContext에서 user 확인
     const gqlContext = GqlExecutionContext.create(context).getContext();
     // http request를 gql로 변환
+    // app.module에 설정하고 gql이 request할 때 포착
     const user = gqlContext['user'];
+    // 위 변수가 있다면 유저가 로그인했다는 것임.
     // console.log(user);
     if (!user) {
       return false;
       // request can't continue
     }
+
+    /* 이하부터는 metadata도 있고, 유저도 로그인이 되었을 경우 */
     if (roles.includes('Any')) {
       return true;
       // request continue
--- src/auth/role.decorator.ts
+++ src/auth/role.decorator.ts
@@ -4,3 +4,4 @@ import { UserRole } from 'src/users/entities/user.entity';
 export type AllowedRoles = keyof typeof UserRole | 'Any';
 export const Role = (roles: AllowedRoles[]) => SetMetadata('roles', roles);
 // 'roles' 키와 role value로 roles metadata에 등록
+// resolver에 metadata나 role이 없으면 그 resolver는 public(권한없어도 됨)으로 간주됨
