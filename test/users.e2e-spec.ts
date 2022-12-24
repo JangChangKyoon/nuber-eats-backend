23be70377269a89da6aca9bf041ea00b82d0178a
JangChangKyoon <58103130+JangChangKyoon@users.noreply.github.com>
2022-12-25 04:27:49 +0900

    e2eTest setting 2

--- test/users.e2e-spec.ts
+++ test/users.e2e-spec.ts
@@ -2,6 +2,7 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
+import { DataSource } from 'typeorm';
 
 describe('UserModule (e2e)', () => {
   let app: INestApplication;
@@ -13,6 +14,21 @@ describe('UserModule (e2e)', () => {
 
     app = module.createNestApplication();
     await app.init();
+  });
+
+  afterAll(async () => {
+    const dataSource = new DataSource({
+      type: 'postgres',
+      host: 'localhost',
+      port: +process.env.DB_PORT,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: 'nuber-eats-test',
+    });
+    const connection = await dataSource.initialize();
+    await connection.dropDatabase(); // 테스트 끝나면 디비 지우기
+    await connection.destroy();
+    await app.close(); // test끝나고 app을 종료시키기
   });
 
   it.todo('me');
